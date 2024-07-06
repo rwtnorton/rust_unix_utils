2257ffe6bb757173215e8431071618b110aa4657
Richard W. Norton <rwtnorton@gmail.com>
2024-07-05 19:04:09 -0700

    wrangle clap builder into something that works

--- echo_rust/src/main.rs
+++ echo_rust/src/main.rs
@@ -1,17 +1,40 @@
-use clap::Parser;
+use clap::{Arg, ArgAction, Command};
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Do not print the trailing newline character
-    #[arg(short, default_value_t = false)]
-    n: bool,
-}
-
+// #[derive(Parser, Debug)]
+// #[command(version, about, long_about = None)]
+// struct Cli {
+//     /// Do not print the trailing newline character
+//     #[arg(short, default_value_t = false)]
+//     n: bool,
+// }
 
 fn main() {
     println!("Hello, world!");
-    let args = Args::parse();
-    println!("{:?}", args);
-
+    // let cli = Cli::parse();
+    let matches = Command::new("echo_rust")
+        .bin_name("echo_rust")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("input text")
+                .required(false)
+                .action(ArgAction::Append)
+                .num_args(0..),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .help("do not print the trailing newline character")
+                .short('n')
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
+    // println!("{:?}", cli);
+    println!("{:?}", matches);
+    let omit_newline = matches.get_flag("omit_newline");
+    println!("omit_newline? {}", omit_newline);
+    let arguments: Vec<&String> = match matches.get_occurrences("text") {
+        Some(occ) => occ.flat_map(|hmm| hmm.into_iter()).collect(),
+        None => vec![],
+    };
+    println!("text: {:#?}", arguments);
 }
